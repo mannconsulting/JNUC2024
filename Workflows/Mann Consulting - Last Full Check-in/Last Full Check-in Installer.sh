2bbc930e71d4832b7968242e9f19bc518ab4bc68
isaacatmann <iordonez@mann.com>
2024-09-27 13:14:45 -0400

    Last Full Inventory Upload.

--- Workflows/Mann Consulting - Last Full Check-in/Last Full Check-in Installer.sh
+++ Workflows/Mann Consulting - Last Full Check-in/Last Full Check-in Installer.sh
@@ -24,18 +24,27 @@ read -s jamfpro_password
 jamfpro_url=${jamfpro_url%%/}
 fulltoken=$(curl -s -X POST -u "${jamfpro_user}:${jamfpro_password}" "${jamfpro_url}/api/v1/auth/token")
 authorizationToken=$(plutil -extract token raw - <<< "$fulltoken" )
+baseDir=$(dirname $0)
 
 echo "Uploading categories/Mann Consulting.xml"
-curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/categories" -H "Content-Type: application/xml" -X "POST" --data-binary "@./categories/Mann Consulting.xml"
+curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/categories" -H "Content-Type: application/xml" -X "POST" --data-binary "@$baseDir/categories/Mann Consulting.xml"
+sleep 20
+echo ""
 
 echo "Uploading computerextensionattributes/Mann Consulting - Last Full Check-in.xml"
-curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/computerextensionattributes" -H "Content-Type: application/xml" -X "POST" --data-binary "@./computerextensionattributes/Mann Consulting - Last Full Check-in.xml"
+curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/computerextensionattributes" -H "Content-Type: application/xml" -X "POST" --data-binary "@$baseDir/computerextensionattributes/Mann Consulting - Last Full Check-in.xml"
+echo ""
+sleep 20
 
 echo "Uploading computergroups/Mann Consulting - Last Full Check-in = Fail.xml"
-curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/computergroups" -H "Content-Type: application/xml" -X "POST" --data-binary "@./computergroups/Mann Consulting - Last Full Check-in = Fail.xml"
+curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/computergroups" -H "Content-Type: application/xml" -X "POST" --data-binary "@$baseDir/computergroups/Mann Consulting - Last Full Check-in = Fail.xml"
+sleep 20
+echo ""
 
 echo "Uploading computergroups/Mann Consulting - Last Full Check-in = Pass.xml"
-curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/computergroups" -H "Content-Type: application/xml" -X "POST" --data-binary "@./computergroups/Mann Consulting - Last Full Check-in = Pass.xml"
+curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/computergroups" -H "Content-Type: application/xml" -X "POST" --data-binary "@$baseDir/computergroups/Mann Consulting - Last Full Check-in = Pass.xml"
+sleep 20
+echo ""
 
 echo "Uploading policies/zzzz Mann Consulting - Policies Finished.xml"
-curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/policies" -H "Content-Type: application/xml" -X "POST" --data-binary "@./policies/zzzz Mann Consulting - Policies Finished.xml"
+curl -sS -H "Authorization: Bearer $authorizationToken" "$jamfpro_url/JSSResource/policies" -H "Content-Type: application/xml" -X "POST" --data-binary "@$baseDir/policies/zzzz Mann Consulting - Policies Finished.xml"
